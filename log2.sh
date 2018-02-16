c5fd7f797687d1855e2b65c0e5a474d0d2b44b37
cainesmckoy <caine12393@yahoo.com>
2018-02-16 11:31:12 -0500

    Update log2.sh

--- log2.sh
+++ log2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # THIS IS A SCRIPT TO GRAB IMPORTANT INFO FROM LOG FILES
+#COPY SCRIPT AS log2-1.sh
+#REMOVE -1 in log2-1.sh
+
+sudo pkill -9 log2-1.sh
 NOW=$( date '+%s' )
 REF=$(( $NOW - 15*60 ))
 
@@ -13,4 +17,4 @@ while read MONTH DAY HMS A4 ; do
 
 done < /var/log/auth.log | grep 'Accept'
 sleep 15
-sudo ./log2.sh
+sudo ./log2-1.sh
