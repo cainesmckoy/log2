3f29c10cf03b7731c02bff8670a505926116ef0e
cainesmckoy <caine12393@yahoo.com>
2018-12-15 09:20:40 -0500

    Update log2.sh

--- log2.sh
+++ log2.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 # THIS IS A SCRIPT TO GRAB IMPORTANT INFO FROM LOG FILES
-#COPY SCRIPT AS log2-1.sh
-#REMOVE -1 in log2-1.sh
 
-sudo pkill -9 log2-1.sh
 NOW=$( date '+%s' )
 REF=$(( $NOW - 15*60 ))
 
+
+while true; do
 while read MONTH DAY HMS A4 ; do
 
-	TIMESTAMP=$(date --date "${MONTH} ${DAY} ${HMS}" '+%s')
+	TIMESTAMP=$(date --date "${MONTH} ${DAY} ${HMS} ${A4}" '+%s')
 	if [[ "$TIMESTAMP" -ge "$REF" ]] ; then
 		echo "${MONTH} ${DAY} ${HMS} ${A4}"
 			
 	fi
 
-done < /var/log/auth.log | grep 'Accept'
-sleep 15
-sudo ./log2-1.sh
+done < /var/log/auth.log | tail | grep 'Accept'
+sleep 10
+done
