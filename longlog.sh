fb935fa4f70eac373d25a6f9bc1ac5480719ce6c
cainesmckoy <caine12393@yahoo.com>
2018-12-15 06:56:00 -0500

    Update longlog.sh

--- longlog.sh
+++ longlog.sh
@@ -2,19 +2,62 @@
 #caine munro
 NOW=$( date '+%s' )
 REF=$(( $NOW - 15*60 ))
-while read MONTH DAY  A4 ; do
+while read MONTH DAY HMS A4 ; do
 
-	TIMESTAMP=$(date --date "${MONTH} ${DAY} " '+%s')
+	TIMESTAMP=$(date --date "${MONTH} ${DAY} ${HMS} ${A4}" '+%s')
 	if [[ "$TIMESTAMP" -ge "$REF" ]] ; then
-		echo "${MONTH} ${DAY}  ${A4}"
+		echo "${MONTH} ${DAY} ${HMS} ${A4}"
 			
 	fi
 #THe access and erros logs had to be set to chmod 660
-echo "errors" >> final_log.txt
-sudo grep 'error' | /var/log/apache2/access_log >> final_log.txt
+the script always deletes the previous log
+sudo rm final_log.txt
+echo "errors" > final_log.txt
+echo "--------------------------------------------------------------------------------------------"
+sudo grep 'error' /var/log/secure | sort -u >> final_log.txt
 echo "warnings" >> final_log.txt
-sudo grep 'warning' | /var/log/apache2/access_log >> final_log.txt
-echo "errors" >> final_log.txt
-sudo grep 'error' | /var/log/apache2/error_log >> final_log.txt
+echo "--------------------------------------------------------------------------------------------"
+
+sudo grep 'warning'  /var/log/secure | sort -u >> final_log.txt
+
+echo "errors" > final_log.txt
+echo "--------------------------------------------------------------------------------------------"
+
+sudo grep 'error' /var/log/cron | sort -u >> final_log.txt
+
 echo "warnings" >> final_log.txt
-done < /var/log/apache2/error_log | grep 'warning' >> final_log.txt
+echo "--------------------------------------------------------------------------------------------"
+
+sudo grep 'warning'  /var/log/cron | sort -u >> final_log.txt
+
+echo "errors" > final_log.txt
+echo "--------------------------------------------------------------------------------------------"
+
+sudo grep 'error' /var/log/messages | sort -u >> final_log.txt
+echo "warnings" >> final_log.txt
+echo "--------------------------------------------------------------------------------------------"
+
+sudo grep 'warning'  /var/log/messages | sort -u >> final_log.txt
+
+echo "errors" > final_log.txt
+echo "--------------------------------------------------------------------------------------------"
+
+sudo grep 'error' /var/log/syslog| sort -u >> final_log.txt
+echo "warnings" >> final_log.txt
+echo "--------------------------------------------------------------------------------------------"
+
+sudo grep 'warning'  /var/log/syslog | sort -u >> final_log.txt
+echo "errors" > final_log.txt
+echo "--------------------------------------------------------------------------------------------"
+
+sudo grep 'error' /var/log/apache2/access.log | sort -u >> final_log.txt
+echo "warnings" >> final_log.txt
+echo "--------------------------------------------------------------------------------------------"
+
+sudo grep 'warning'  /var/log/apache2/access.log | sort -u >> final_log.txt
+echo "errors" >> final_log.txt
+echo "--------------------------------------------------------------------------------------------"
+
+sudo grep 'error'  /var/log/apache2/error.log | sort -u >> final_log.txt
+echo "warnings" >> final_log.txt
+done < /var/log/apache2/error.log | grep 'warning' | sort -u >> final_log.txt
